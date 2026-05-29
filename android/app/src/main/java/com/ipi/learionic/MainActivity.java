1ba21e7f4704df7c126e102e30ff65437542bc4a
Iman Sanjaya <imansanjaya800@gmail.com>
2026-05-30 00:45:28 +0700

    Menambahkan Durasi SplashScreen saat muncul dengan mengubah code MainActivity.java di project Android

--- android/app/src/main/java/com/ipi/learionic/MainActivity.java
+++ android/app/src/main/java/com/ipi/learionic/MainActivity.java
@@ -1,5 +1,22 @@
 package com.ipi.learionic;
 
+import android.os.Bundle;
+import android.os.SystemClock;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final long SPLASH_DURATION_MS = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        long splashStartedAt = SystemClock.uptimeMillis();
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        splashScreen.setKeepOnScreenCondition(() ->
+            SystemClock.uptimeMillis() - splashStartedAt < SPLASH_DURATION_MS
+        );
+
+        super.onCreate(savedInstanceState);
+    }
+}
