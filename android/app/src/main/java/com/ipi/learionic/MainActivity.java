49b2e6b8ec05901f95fefa5f0d2d0a6365b9caba
Iman Sanjaya <imansanjaya800@gmail.com>
2026-05-30 00:03:52 +0700

    Add Build IONIC Project Ke Android Studio agar bisa dibuat APK/AAB

---
+++ android/app/src/main/java/com/ipi/learionic/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ipi.learionic;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ipi.learionic',
   appName: 'LearnIonic',
   webDir: 'www'
 };
