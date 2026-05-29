9f736880dc7a1bcabaab4fa20ed2656a5dbcb868
Iman Sanjaya <imansanjaya800@gmail.com>
2026-05-30 00:49:19 +0700

    Menambahkan Back Button bawaan Android agar men-trigger flow Page akses yang Kita inginkan termasuk untuk melakukan kill aplikasi di android dengan CapacitorApp.exitApp()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { App as CapacitorApp } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private router: Router
+  ) {
+    this.initializeBackButtonHandler();
+  }
+
+  private initializeBackButtonHandler() {
+    this.platform.ready().then(() => {
+      this.platform.backButton.subscribeWithPriority(10, () => {
+        const currentPath = this.router.url.split('?')[0].split('#')[0];
+
+        if (currentPath === '/welcome' || currentPath === '/home' || currentPath === '/') {
+          CapacitorApp.exitApp();
+          return;
+        }
+
+        this.router.navigateByUrl('/home', { replaceUrl: true });
+      });
+    });
+  }
 }
